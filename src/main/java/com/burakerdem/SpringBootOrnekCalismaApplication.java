350ae16eee77ed8e440832d371bdb942bfe095d0
erdemburak <burakerdem6@hotmail.com>
2022-10-18 16:36:38 +0300

    first commit

---
+++ src/main/java/com/burakerdem/SpringBootOrnekCalismaApplication.java
@@ -0,0 +1,13 @@
+package com.burakerdem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOrnekCalismaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOrnekCalismaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burakerdem/SpringBootOrnekCalismaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakerdem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOrnekCalismaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
